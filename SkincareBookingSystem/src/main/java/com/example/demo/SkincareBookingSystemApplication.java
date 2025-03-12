8151f91d22f97c2103b709f33e951bb2b1bbf2e2
VU DUC DUNG <vuducdung0379@gmail.com>
2025-03-12 19:31:28 +0700

    Create source

---
+++ SkincareBookingSystem/src/main/java/com/example/demo/SkincareBookingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkincareBookingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkincareBookingSystemApplication.class, args);
+	}
+
+}
