2a0525358acefaa2db4600f70902f723a627f2c9
Trung Khanh <trungkhanh253@gmail.com>
2025-03-17 23:04:10 +0700

    Custom Admin page

--- SkincareBookingSystem/src/main/java/com/example/Services/SkincareBookingSystemApplication.java
+++ SkincareBookingSystem/src/main/java/com/example/Services/SkincareBookingSystemApplication.java
@@ -2,8 +2,10 @@ package com.example.Services;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.example")
 public class SkincareBookingSystemApplication {
 
 	public static void main(String[] args) {
