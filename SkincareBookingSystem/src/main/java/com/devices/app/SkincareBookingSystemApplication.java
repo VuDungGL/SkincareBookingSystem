c243bcf9c5c0424f238c3a53c877c5b0e4d4c81f
huylam0703 <huylamnhat244@gmail.com>
2025-04-22 19:35:14 +0700

    infoUser

--- SkincareBookingSystem/src/main/java/com/devices/app/SkincareBookingSystemApplication.java
+++ SkincareBookingSystem/src/main/java/com/devices/app/SkincareBookingSystemApplication.java
@@ -8,21 +8,11 @@ import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan("com.devices")
-public class SkincareBookingSystemApplication implements CommandLineRunner {
+public class SkincareBookingSystemApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SkincareBookingSystemApplication.class, args);
 	}
 
-	@Override
-	public void run(String... args) throws Exception {
-		// Mã hóa mật khẩu khi ứng dụng khởi động
-		String rawPassword = "hashed_admin02";
-		String encodedPassword = HashUtil.encodePassword(rawPassword);
 
-		String encodedPassword2 = HashUtil.encodePassword("hashed_member03");
-		// In ra mật khẩu đã mã hóa
-		System.out.println("Mã hóa mật khẩu: " + encodedPassword);
-		System.out.println("Mã hóa mật khẩu: " + encodedPassword2);
-	}
 }
